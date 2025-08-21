48eee6a72bb1181f7f9c288d8e8b773a20f1398b
Zaviyar Ansari <100063933+Zaviyar-Ansari@users.noreply.github.com>
2025-08-21 18:01:45 +0500

    changes

--- backend/src/supabaseClient.ts
+++ backend/src/supabaseClient.ts
@@ -3,10 +3,11 @@
  * 
  * See the LICENSE file for more information.
  */
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js'
 
-import { createClient } from "@supabase/supabase-js";
+// Replace with your values
+const SUPABASE_URL = "https://jrrgkhxtlrwpiydzhaxo.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpycmdraHh0bHJ3cGl5ZHpoYXhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5ODgyMzcsImV4cCI6MjA3MDU2NDIzN30.VXPLugxKZSKBSDno98fYf3PzLhFmBlMetydXGNSWrss"
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
